087965f64a81398a543ef8d850f1cf18d7162e59
seelder <seelder@ncsu.edu>
2020-03-03 23:22:01 -0500

    Removed unused work

--- RequirementsParser/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into classification(classification_name) values ('confidentiality');
-insert into classification(classification_name) values ('integrity');
-insert into classification(classification_name) values ('availability');
-insert into classification(classification_name) values ('idauth');
-insert into classification(classification_name) values ('accountability');
-insert into classification(classification_name) values ('privacy');
